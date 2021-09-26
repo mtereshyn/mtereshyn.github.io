f2cea9c3b778d9d927d42bb8d063a30d0c62656a
mtereshyn <maksym.tereshyn@nure.ua>
2021-09-26 16:20:55 +0300

    new portfolio

--- js/main.js
+++ js/main.js
@@ -13,44 +13,35 @@ hamb.addEventListener("click", function () {
   }
 });
 
-
-/* Устанавливаем стартовый индекс слайда по умолчанию: */
 let slideIndex = 1;
-/* Вызываем функцию, которая реализована ниже: */
+
 showSlides(slideIndex);
 
-/* Увеличиваем индекс на 1 — показываем следующий слайд: */
 function nextSlide() {
-    showSlides(slideIndex += 1);
+  showSlides((slideIndex += 1));
 }
-
-/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
 function previousSlide() {
-    showSlides(slideIndex -= 1);  
+  showSlides((slideIndex -= 1));
 }
 
-/* Устанавливаем текущий слайд: */
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
-
-/* Функция перелистывания: */
 function showSlides(n) {
-    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-    let slides = document.getElementsByClassName("projects--wrapper__slider--item");
-    
-    /* Проверяем количество слайдов: */
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-  
-    /* Проходим по каждому слайду в цикле for: */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
-    /* Делаем элемент блочным: */
-    slides[slideIndex - 1].style.display = "flex";    
+  let slides = document.getElementsByClassName(
+    "projects--wrapper__slider--item"
+  );
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
 }

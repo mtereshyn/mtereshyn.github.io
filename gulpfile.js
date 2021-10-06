d6093673a4d8ddc74d7ba9c43ad4222cb17fc798
mtereshyn <maksym.tereshyn@nure.ua>
2021-10-06 18:43:18 +0300

    gulpfile better with knowledge

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+
+const {src, dest, parallel, series, watch} = require('gulp');
+const browserSync  = require('browser-sync');
+const concat       = require('gulp-concat');
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const sass         = require('gulp-sass')(require('sass'));
+const cleancss     = require('gulp-clean-css')
+
+
+function browsersync() {
+  browserSync.init({
+    server: {baseDir: 'app/' },
+    notify: false,
+    online: true
+  })
+}
+
+function styles() {
+  return src('app/sass/main.sass')
+  .pipe(sass())
+  .pipe(concat('app.min.css'))
+  .pipe(autoprefixer({ overrideBrowserlist: ['last 10 versions'], grid: true}))
+  .pipe(cleancss)
+  .pipe(dest('app/css/'))
+} 
+
+
+function scripts(){
+  return src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'app/js/app.js',
+  ])
+  .pipe(concat('app.min.js'))
+  .pipe(uglify())
+  .pipe(dest('app/js/'))
+  .pipe(browserSync.stream())
+}
+
+function startWatch() {
+  watch(['app/**/*.js', '!app/**/*.min.js'], scripts)
+}
+
+exports.browsersync = browsersync;
+exports.scripts     = scripts;
+exports.styles      = styles;
+
+exports.default     = parallel(scripts, browsersync, startWatch);
